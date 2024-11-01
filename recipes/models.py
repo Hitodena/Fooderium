a9824d4f60e83acbaa3bcd6e5131ce4abba4226b
Hitodena <Hitodena@ya.ru>
2024-11-01 23:41:01 +0300

    [RECIPES] create model | [USERS] update based on recipes, notifications | [NOTIFICATIONS] update based on recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+from notifications.models import Comment
+from products.models import Product
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="recipes/", blank=True)
+    description = models.TextField()
+    calories = models.PositiveIntegerField()
+    proteins = models.PositiveIntegerField()
+    fats = models.PositiveIntegerField()
+    carbs = models.PositiveIntegerField()
+    spiciness = models.IntegerField(blank=True, null=True)
+    difficulty = models.CharField(max_length=50, blank=True)
+    total_time = models.DurationField()
+    kitchen_time = models.DurationField(blank=True, null=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    favorites_count = models.PositiveIntegerField(default=0, blank=True)
+    tags = TaggableManager()
+    # Many-to-many fields
+    products = models.ManyToManyField(Product, blank=True)
+    comments = models.ManyToManyField(Comment, blank=True)
+
+
+class RecipeStep(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name="steps", on_delete=models.CASCADE)
+    step_number = models.PositiveIntegerField()
+    instruction = models.TextField()
+    image = models.ImageField(upload_to="recipe-steps-images/", blank=True)
+
+    class Meta:
+        ordering = ["step_number"]
--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from recipes.models import Recipe
+from notifications.models import Comment
 
 
 class UserProfile(models.Model):
@@ -7,5 +9,5 @@ class UserProfile(models.Model):
     nickname = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=20, blank=True)
     profile_image = models.ImageField(upload_to="profile-images/", blank=True)
-    # favorite_recipes = models.ManyToManyField(Recipe, blank=True, related_name="favorited_by")
-    # comments = models.ManyToManyField(Comment, blank=True, related_name="user_comments")
+    favorite_recipes = models.ManyToManyField(Recipe, blank=True, related_name="favorited_by")
+    comments = models.ManyToManyField(Comment, blank=True, related_name="user_comments")
