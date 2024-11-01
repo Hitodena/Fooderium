f20cfe24bdea88fdfc14e710bbc6f8ebbe4b3ef6
Hitodena <Hitodena@ya.ru>
2024-11-01 23:31:50 +0300

    [NOTIFICATIONS] create model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe_comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self) -> str:
+    #     return f"Comment by {self.user.username} on {self.recipe.title}"
