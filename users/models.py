71286ce50f42bc880aa6025f67cb2c34000e5758
Hitodena <Hitodena@ya.ru>
2024-11-01 16:28:39 +0300

    [USERS] create model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    nickname = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20, blank=True)
+    profile_image = models.ImageField(upload_to="profile-images/", blank=True)
+    # favorite_recipes = models.ManyToManyField(Recipe, blank=True, related_name="favorited_by")
+    # comments = models.ManyToManyField(Comment, blank=True, related_name="user_comments")
