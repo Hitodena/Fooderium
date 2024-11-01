31635b488027b95a4af0785c3ced9bca66afff1e
Hitodena <Hitodena@ya.ru>
2024-11-01 23:25:00 +0300

    [PRODUCTS] create model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="product-images/")
+    calories = models.PositiveIntegerField()
+    proteins = models.PositiveIntegerField()
+    fats = models.PositiveIntegerField()
+    carbs = models.PositiveIntegerField()
+    history = models.TextField(blank=True)
+    benefits = models.TextField(blank=True)
+    harms = models.TextField(blank=True)
+    tags = TaggableManager()
