445bafc41a8730dff676d24c8dcec97d4fb115e3
Hitodena <Hitodena@ya.ru>
2024-11-03 11:39:17 +0300

    [BUILD] cors error fix

--- config/django/base.py
+++ config/django/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "products",
     "recipes",
     "notifications",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -109,6 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# CORS
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # REST
 
