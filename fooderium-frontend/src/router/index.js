8c8e7daaa6fb55ad83b3b26d17fcdc57d518fe69
Hitodena <Hitodena@ya.ru>
2024-11-03 10:09:29 +0300

    [DESIGN] delete old

--- fooderium-frontend/src/router/index.js
+++ fooderium-frontend/src/router/index.js
@@ -1,16 +1,10 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Recipes from '../views/TheRecipes.vue';
-import RecipeDetail from '../views/RecipeDetail.vue';
+import { createRouter, createWebHistory } from 'vue-router'
 
-Vue.use(Router);
+const routes = []
 
-const routes = [
-  { path: '/recipes', component: Recipes },
-  { path: '/recipe/:id', component: RecipeDetail, props: true },
-];
-
-export default new Router({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
+
+export default router
